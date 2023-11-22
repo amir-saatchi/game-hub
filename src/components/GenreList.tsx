8e42c5432885e31c4bca70c281ffcf94a5d93c5b
Amir Saatchi <Amir17252@gmail.com>
2023-11-22 19:54:38 +0330

    Display Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenre";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, isLoading, error } = useGenres();
   return (
-    <ul>
-      {data.map((item) => (
-        <li key={item.id}>{item.name}</li>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize='lg'>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
